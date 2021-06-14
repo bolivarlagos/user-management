618199021b7fe0e2bc8b369e591f5dac6d1d0a15
bolivarlagos <bolivarlagos@gmail.com>
2021-06-14 11:58:10 -0300

    better with async/await

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,50 +3,68 @@ const messages = require('../messages')
 const crypto = require('crypto-js/sha256')
 const Mongo = require('../models/mongoSchema')
 
+
 module.exports.mongoConnection = () => {
     return mongoose.connect(messages.URI, { useNewUrlParser: true, useUnifiedTopology: true})
 }
 
-module.exports.users = (req, res) => {
-    Mongo.find()
-    .then(users => {  
-        let new_user = []   
-        users.map(user => {                        
-            let { _id, name, surname } = user
-            new_user.push({ _id, name, surname })           
-        })  
-        res.json(new_user)
-    })    
-    .catch(error => console.log(error))    
+module.exports.users = async (req, res) => {
+     try {
+          const users = await Mongo.find()
+          let new_user = []
+          await users.map(user => {
+               let { _id, name, surname } = user
+               new_user.push({ _id, name, surname }) 
+          })
+          res.status(200).json(new_user)
+          
+     } catch (error) {
+          res.status(500).json({ message: error })         
+     }  
 }
 
-module.exports.singleUser = (req, res) => {
-    Mongo.findById(req.params.id)
-    .then((user) => {
-        let { _id, name, surname } = user
-        res.json({_id, name, surname})
-    })
-    .catch(error => console.log(error))
+module.exports.singleUser = async (req, res) => {
+     try{
+          const user = await Mongo.findById(req.params.id)
+          let {_id, name, surname } =  await user
+          res.status(200).json({ _id, name, surname })
+     } catch (error) {
+          res.status(500).json({ message: error })
+     } 
 }
 
-module.exports.postUser = (req, res) => {
-    let { name, surname, password } = req.body
-    password = crypto(password).toString()
-    let user = new Mongo({ name, surname, password})
-    user.save()
-    .then((user) => console.log(user))
-    .catch(error => console.log(error))
+module.exports.postUser = async (req, res) => {
+     try {
+          let { name, surname, password } = req.body
+          password = crypto(password).toString()
+          let user = new Mongo({ name, surname, password })
+          await user.save()
+          let {_id } = await user
+          res.status(200).json({ _id, name, surname })
+          
+     } catch (error) {
+          console.log(error)
+          res.status(500).json({ message: error })          
+     }   
+
 }
 
-module.exports.deleteUser = (req, res) => {
-    console.log(req.params.id)
-    Mongo.deleteOne({ _id: req.params.id })
-    .then(() => console.log(messages.userDeleted))
-    .catch(error => console.log(error))
+module.exports.deleteUser = async (req, res) => {    
+     try {
+          const user = await Mongo.deleteOne({ _id: req.params.id })
+          res.status(200).json({message: "user deleted"})
+          
+     } catch (error) {
+          res.status(500).json({ message: error })          
+     }
 }
 
-module.exports.updateUser = (req, res) => {
-    Mongo.updateOne({ _id: req.params.id }, req.body)
-    .then(() => console.log(messages.userUpdated))
-    .catch(error => console.log(error))
+module.exports.updateUser = async (req, res) => {
+     try {
+          const user = await Mongo.updateOne({ _id: req.params.id }, req.body)
+          res.status(200).json({user})
+          
+     } catch (error) {
+          res.status(500).json({ message: error })          
+     }
 }
